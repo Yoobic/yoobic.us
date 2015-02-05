e1756b69bf74e5e7cc388a4d641b5e4ede7a52c7
Joshua Estrin Skrzypek <jskrzypek@gmail.com>
2015-02-05 19:43:13 +0200

    feat(yooSlide): Manually transclude consumer content with wrapped fa-surface if needed

--- src/scripts/core/directives/faSlide.js
+++ src/scripts/core/directives/faSlide.js
@@ -18,6 +18,33 @@ module.exports = function(app) {
     // directive
     var directiveDeps = [];
     var directive = function() {
+
+        /**
+         * Transclude the contents provided by the consumer into the template and
+         * append it to the tElement.
+         * @param {string|Element} template - the template for the slide with one fa-surface and ng-transclude
+         * @param {Element} tElement - the tElement passed to the compile function containing the consumer's slide contents
+         * @param {bool} wrapWithSurface - a boolean indicating whether the consumer's contents need the fa-surface wrapper from the template
+         */
+        var transcludeConsumerProvidedContents = function(template, tElement, wrapWithSurface) {
+            template = angular.element(template); // make sure template is an angular element
+
+            if(wrapWithSurface) {
+                tElement.append(template
+                    .find('ng-transclude') // find the the template's ng-transclude
+                    .parent() // select the parent of the template's ng-transclude (the fa-surface)
+                    .empty() // delete the selected element's contents
+                    .append(tElement.contents())); // append the consumer contents from tElement
+            } else {
+                tElement.append(template
+                    .find('ng-transclude') // find the the template's ng-transclude
+                    .parent() // select the parent of the template's ng-transclude (the fa-surface)
+                    .parent() // select the parent of the template's fa-surface
+                    .empty() // delete the selected element's contents
+                    .append(tElement.contents())); // append the consumer contents from tElement
+            }
+        };
+
         return {
             require: ['faSlide', '^faSlideBox'],
             restrict: 'AE',
@@ -25,10 +52,12 @@ module.exports = function(app) {
             controller: controller,
             controllerAs: 'faSlideCtrl',
             bindToController: true,
-            template: require('./faSlide.html'),
-            transclude: true,
             compile: function(tElement, tAttrs) {
+                var template = require('./faSlide.html');
+                var wrapWithSurface = tElement.find('fa-surface').length ? false : true;
+                transcludeConsumerProvidedContents(template, tElement, wrapWithSurface);
                 return {
+
                     pre: function(scope, element, attrs, ctrls) {},
                     post: function(scope, element, attrs, ctrls) {
                         var faSlideCtrl = ctrls[0];
